cb16a3e0624164200b0da8d9de1258805cf2b51d
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2023-09-05 15:44:36 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/projeto_guit/Projeto_guit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_guit;
+
+/**
+ *
+ * @author brian.7908
+ */
+public class Projeto_guit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
